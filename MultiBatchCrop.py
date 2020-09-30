c45adf7d1e5cad33c130fbe5de11f85e23165118
fishermess <992492046@qq.com>
2020-09-30 19:05:25 +0800

    final

--- MultiBatchCrop.py
+++ MultiBatchCrop.py
@@ -5,25 +5,26 @@ import os
 from tqdm import tqdm
 if __name__ == "__main__":
     fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D,device='cpu',flip_input=False)
-    filename_list = os.listdir("../pg_data/")
+    filename_list = os.listdir("./pg_data/")
     #print(filename_list)
     for filename in tqdm(filename_list):
         try:
-            input = io.imread('../pg_data/'+filename)
+            input = io.imread('./pg_data/'+filename)
             preds = fa.get_landmarks(input)
             #print(preds[0])
-            img = cv2.imread('../pg_data/'+filename)
+            img = cv2.imread('./pg_data/'+filename)
             #x= (max([i[0] for i in preds[0]])+min([i[0] for i in preds[0]]))/2
             #y= (max([i[1] for i in preds[0]])+min([i[1] for i in preds[0]]))/2
             x1 = max(i[0] for i in preds[0])
             x0 = min(i[0] for i in preds[0])
             y1 = max(i[1] for i in preds[0])
             y0 = min(i[1] for i in preds[0])
+            y = (y0+y1)/2
             print(x0,x1,y0,y1)
             print(filename.split("/")[-1])
-            cropped = img[0:int(y1)+10,0:img.shape[1]]  # 裁剪坐标为[y0:y1, x0:x1]
-            cv2.imwrite("../pg_data_crop/"+filename.split(".")[0]+"-crop.jpg", cropped)
+            length = img.shape[1]
+            cropped = img[int(y-length/2):int(y+length/2)+10,0:img.shape[1]]  # 裁剪坐标为[y0:y1, x0:x1]
+            cv2.imwrite("./pg_data_crop/"+filename.split(".")[0]+"-crop.jpg", cropped)
         except:
             print("ERROR")
             continue
-
